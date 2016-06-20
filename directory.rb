5ffcd4c477b6b102650ae8df9ca551f88abaf2d2
Harry Jaggs <harryjaggs92@gmail.com>
2016-06-20 14:25:57 +0000

    coding cleaning user input

--- directory.rb
+++ directory.rb
@@ -1,13 +1,18 @@
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit enter twice"
+    # create an empty array
     students = []
     name=gets.chomp
+    # while the name is not empty, repeat this code
     while !name.empty? do
+        # add the student hash to the array
         students << {name: name, cohort: :november}
         puts "Now we have #{students.count} students"
+        # get another name from the user
         name = gets.chomp
-end  
+end
+# return the array of students
 students
 end
 
@@ -26,9 +31,8 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-
-#nothing happens until we call the methods
 students = input_students
+#nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
