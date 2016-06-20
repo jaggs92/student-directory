0a9e9f5d376ed2cb587b06369edfb353ae87b43e
Harry Jaggs <harryjaggs92@gmail.com>
2016-06-20 13:29:23 +0000

    array.each added

--- directory.rb
+++ directory.rb
@@ -10,20 +10,11 @@ students =  ["Dr. Hannibal Lecter",
  "The Joker",
  "Joffrey Baratheon",
  "Norman Bates"]
-
 puts "The students of Villians Academy"
 puts "-------------"
-puts students [0]
-puts students [1]
-puts students [2]
-puts students [3]
-puts students [4]
-puts students [5]
-puts students [6]
-puts students [7]
-puts students [8]
-puts students [9]
-puts students [10]
+students.each do |student|
+    puts student
+end
 # finally, we print the total number of students
 puts "Overall, we have #{students.count} great students"
-# it's important that print () doesn't add new line characters
+
