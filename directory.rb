e99380ac3c73875f18bdab9cf299bf19ae89de21
Harry Jaggs <harryjaggs92@gmail.com>
2016-06-21 09:44:04 +0000

    each_with_index added

--- directory.rb
+++ directory.rb
@@ -11,7 +11,7 @@ def input_students
         puts "Now we have #{students.count} students"
         # get another name from the user
         name = gets.chomp
-end
+    end
 # return the array of students
 students
 end
@@ -22,8 +22,8 @@ def print_header
 end
 
 def print(students)
-    students.each do |student|
-    puts "#{student [:name]} (#{student[:cohort]} cohort)"
+    students.each_with_index do |student, i|
+    puts "#{i+1}. #{student [:name]} (#{student[:cohort]} cohort)"
   end
 end
 
