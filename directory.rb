082bdcf11b1ac4305be666a2bb371c331be4a699
Harry Jaggs <harryjaggs92@gmail.com>
2016-06-27 08:52:34 +0000

    code changes

--- directory.rb
+++ directory.rb
@@ -1,30 +1,26 @@
 def interactive_menu
-   students = []
-   loop do
-      #1. Present the options
-      puts "1. Input the students"
-      puts "2. Show the students"
-      puts "9. Exit"
-      #2. Store result as a variable
-      selection = gets.chomp
-      #3. Do what the user has asked
-      case selection
-      when "1"
-         students = input_students
-      when "2"
-         # Show the students
-         print_header
-         cohort_months = cohorts(students)
-         print(students, cohort_months)
-         print_footer(students)
-      when "9"
-         # exit
-         exit
-      else
-         "I didn't catch that. Could you try again please"
-      end
-   end
-
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
 end
 
 def input_students
